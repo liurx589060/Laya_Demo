7223d0859837feaebf95d5661c46ccd9082a4810
daven.liu <liurx589060@163.com>
2018-03-21 17:47:21 +0800

    1

--- src/ui/layaUI.max.all.js
+++ src/ui/layaUI.max.all.js
@@ -18,6 +18,6 @@ var testUI=(function(_super){
 
 		}
 
-		testUI.uiView={"type":"View","props":{"width":600,"height":400},"child":[{"type":"Image","props":{"y":64,"x":49,"width":256,"skin":"comp/bg.png","height":199}},{"type":"Button","props":{"y":239,"x":277,"width":211,"skin":"comp/button.png","sizeGrid":"0,0,0,0","rotation":0,"labelSize":50,"labelFont":"SimSun","labelColors":"#a2b546","label":"我的你","height":118}}]};
+		testUI.uiView={"type":"View","props":{"width":600,"height":400},"child":[{"type":"Image","props":{"y":64,"x":49,"width":256,"skin":"comp/bg.png","height":199}},{"type":"Button","props":{"y":236,"x":71,"width":211,"skin":"comp/button.png","sizeGrid":"0,0,0,0","rotation":0,"labelSize":50,"labelFont":"SimSun","labelColors":"#a2b546","label":"我的你","height":118}},{"type":"ProgressBar","props":{"y":298,"x":347,"width":212,"skin":"comp/progress.png","pivotY":1,"pivotX":8,"height":14}}]};
 		return testUI;
 	})(View);
